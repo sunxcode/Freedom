8e8ecd0687b53a30ba0ec7314f9ccd80ec0aa333
Super <2829969299@qq.com>
2018-07-04 18:04:29 +0800

    update

--- Freedom/utility/NSObject+Freedom.h
+++ Freedom/utility/NSObject+Freedom.h
@@ -6,3 +6,35 @@
 #import <Foundation/Foundation.h>
 @interface NSObject (Freedom)
 @end
+@interface UIViewController (DismissKeyboard)
+-(void)setupForDismissKeyboard;
+@end
+@implementation UIViewController (DismissKeyboard)
+- (void)setupForDismissKeyboard {
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    UITapGestureRecognizer *singleTapGR =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(tapAnywhereToDismissKeyboard:)];
+
+    __weak UIViewController *weakSelf = self;
+
+    NSOperationQueue *mainQuene =[NSOperationQueue mainQueue];
+    [nc addObserverForName:UIKeyboardWillShowNotification
+                    object:nil
+                     queue:mainQuene
+                usingBlock:^(NSNotification *note){
+                    [weakSelf.view addGestureRecognizer:singleTapGR];
+                }];
+    [nc addObserverForName:UIKeyboardWillHideNotification
+                    object:nil
+                     queue:mainQuene
+                usingBlock:^(NSNotification *note){
+                    [weakSelf.view removeGestureRecognizer:singleTapGR];
+                }];
+}
+
+- (void)tapAnywhereToDismissKeyboard:(UIGestureRecognizer *)gestureRecognizer {
+    //此method会将self.view里所有的subview的first responder都resign掉
+    [self.view endEditing:YES];
+}
+@end
